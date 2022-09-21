073a8e85e000bb6fdb6d79ad0491452f14f28148
paweltheriperr <pawel.programista.kuczmik@gmail.com>
2022-09-21 10:56:38 +0200

    wysylanie json

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-var json
+function wysylkaJSON(){
+
+let name = document.getElementSelector('name');
+
+let xhr = new XMLHttpRequest();
+let url = "database.py";
+xhr.open('GET', url, true);
+xhr.setRequestHeader('Content-Type', 'application/json');
+xhr.onreadystatechange = function(){
+    if (xhr.readyState === 4 && xhr.status ===200){
+        result.innerHTML = JSON.stringify(result);
+    }
+};
+var data =JSON.stringify({"name":name.value});
+}
