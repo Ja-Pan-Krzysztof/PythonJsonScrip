f6bae20c0bffc90f18d5baf2697ed4ee65d06ea0
paweltheriperr <pawel.programista.kuczmik@gmail.com>
2022-09-23 09:54:26 +0200

    zapisywanie python moduł pół działa

---
+++ save.py
@@ -0,0 +1,16 @@
+import json 
+import sqllite3
+connection = sqllite3.connect('ja.db')
+cursor = connection.cursor()
+cursor.execute('Create table if not exists User (name Text,surname Text)')
+
+traffic = json.load(open('json_file.json'))
+columns = ['name','course','roll']
+for row in traffic:
+    keys= tuple(row[c] for c in columns)
+    cursor.execute('insert into User values(?,?,?)',keys)
+    print(f'{row["name"]} data inserted Udało się')
+
+
+connection.commit()
+connection.clase()
