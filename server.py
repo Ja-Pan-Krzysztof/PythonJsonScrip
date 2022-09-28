dd76ec584ce4e3861b370df9500adbbb3926167c
Ja_Pan_Krzysztof <project.django.inne@gmail.com>
2022-09-28 12:28:24 +0200

    little corrections

---
+++ server.py
@@ -0,0 +1,98 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+from database import User
+import cgi
+import logging.config
+
+logging.config.fileConfig('logging.conf')
+logger = logging.getLogger(__name__)
+
+''' MySQL
+    :keyword here will be connection with MySQL
+    :keyword and code below will be delete
+'''
+
+db_name = "chuwel"
+
+u = User('ja', 'ja.db')
+u.conn()
+u.create_table()
+u.disconn()
+
+
+class LocalServer(SimpleHTTPRequestHandler):
+    logger.info('Server working')
+
+    @staticmethod
+    def readhtml(path):
+        try:
+            with open(path) as f:
+                file = f.read()
+
+        except Exception as e:
+            file = e
+
+        return file
+
+    def do_GET(self):
+        if self.path == "/":
+
+            template = "./templates/index.html"
+            file = self.readhtml(template)
+            self.send_response(200, "OK")
+            self.end_headers()
+            self.wfile.write(bytes(file, "utf8"))
+
+        if self.path == '/success':
+            pass
+
+    def do_POST(self):#xD
+        if self.path == '/success':#xD
+            form = cgi.FieldStorage(
+                fp=self.rfile,
+                headers=self.headers,
+                environ={'REQUEST_METHOD': 'POST'}
+            )
+
+            name = form.getvalue('name')
+            surname = form.getvalue('surname')
+
+            template = './templates/success.html'
+            file = self.readhtml(template)
+
+            self.send_response(200, 'OK')
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(bytes(file, "utf8"))
+        
+        elif self.path=="/ajax":
+             form = cgi.FieldStorage(
+                fp=self.rfile,
+                headers=self.headers,
+                environ={'REQUEST_METHOD': 'POST'}
+             )
+
+             name = form.getvalue('name')
+             surname = form.getvalue('surname')
+       #    form = cgi.FieldStorage(
+       #       fp=self.rfile,
+       #       headers=self.headers,
+       #       environ={'REQUEST_METHOD': 'POST'}
+       #   )
+
+       #   name = form.getvalue('name')
+       #   surname = form.getvalue('surname')
+             print(name, surname)
+
+
+class HostServer:
+    def __init__(self, host: str = '192.168.0.111', port: int = 8000):
+        self.port = port
+        self.host = host
+
+    def starhost(self) -> HTTPServer:
+        return HTTPServer((self.host, self.port), LocalServer)
+
+    def stophost(self):
+        return self.starhost()\
+            .shutdown()
